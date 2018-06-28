de0be7b3d8ac5c7882ab8cf0829fcc3a0ddfa38c
Julia Stetskaya <juliastetskaya@gmail.com>
2018-06-28 17:06:58 +0300

    add styles and js script

---
+++ js/script.js
@@ -0,0 +1,56 @@
+var link = document.querySelector('.feedback-link');
+var popup = document.querySelector('.popup-feedback');
+var close = document.querySelector('.popup-close');
+var nameField = popup.querySelector('[name="feedback-name"]');
+var emailField = popup.querySelector('[name="feedback-email"]');
+var textField = popup.querySelector('[name="feedback-text"]');
+var form = popup.querySelector('.feedback-form');
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("feedback-name");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popup.classList.add('popup-show');
+  if (storage) {
+    nameField.value = storage;
+    emailField.focus();
+  } else {
+    nameField.focus();
+  }
+});
+
+close.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popup.classList.remove('popup-show');
+  popup.classList.remove('popup-error');
+});
+
+form.addEventListener('submit', function(evt) {
+  if (!nameField.value || !emailField.value || !textField.value) {
+    evt.preventDefault();
+    popup.classList.remove('popup-error');
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add('popup-error');
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem('feedback-name', nameField.value);
+    }
+  }
+});
+
+window.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains('popup-show')) {
+      popup.classList.remove('popup-show');
+      popup.classList.remove('popup-error');
+    }
+  }
+})
